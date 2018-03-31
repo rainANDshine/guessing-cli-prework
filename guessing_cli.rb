7c8b4383468fb335cfaf5b8c9971c972d1fa1b18
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 09:39:11 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,6 +9,7 @@ def run_guessing_game
       if input == number
         puts "You guessed the correct number!"
       else
-        puts "The computer guessed <number>"
+        puts "The computer guessed #{number}"
+      end
     end
 end
